ed169a08ad1f6bc3e00de1f820800545e7f42039
Radek Ježek <radek.jezek@ibm.com>
2026-02-02 11:07:13 +0100

    fix(cli): refresh token uses wrong http client
    Signed-off-by: Radek Ježek <radek.jezek@ibm.com>

--- apps/agentstack-cli/src/agentstack_cli/auth_manager.py
+++ apps/agentstack-cli/src/agentstack_cli/auth_manager.py
@@ -6,6 +6,7 @@ import typing
 from collections import defaultdict
 from typing import Any
 
+import httpx
 from authlib.common.errors import AuthlibBaseError
 from authlib.integrations.httpx_client import AsyncOAuth2Client
 from authlib.oauth2.rfc6749.errors import InvalidGrantError, OAuth2Error
@@ -63,7 +64,7 @@ class AuthManager:
         if auth_server in self._oidc_cache:
             return self._oidc_cache[auth_server]
 
-        async with AsyncOAuth2Client() as client:
+        async with httpx.AsyncClient() as client:
             try:
                 resp = await client.get(f"{auth_server}/.well-known/openid-configuration")
                 resp.raise_for_status()
